d0383758ae80d2c8c5ed77da6253442efc64d028
caner <canerserbest1997@gmail.com>
2022-01-17 21:27:08 +0300

    hata yönetimi

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*try
+            {
+                 Console.WriteLine("Bir sayı giriniz.");
+                 int sayi = Convert.ToInt32(Console.ReadLine());
+                 Console.WriteLine("Girmiş olduğunuz sayı:" + sayi);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata" + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }*/
+            try
+            {
+                 //int a = int.Parse(null);
+                 //int a = int.Parse("Test");
+                 int a = int.Parse("-200000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş bir değer girdiniz.");
+                Console.Write(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.Write(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(":ok küçük yada çok büyük değer girdiniz.");
+                Console.Write(ex);
+            }
+        }
+    }
+}
